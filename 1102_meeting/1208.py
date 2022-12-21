861fdda82b575f2ea089c18f56a99e11a0ce20e9
JamalXuan <102579581+JamalXuan@users.noreply.github.com>
2022-12-21 12:25:46 +0800

    Update 1208.py

--- 1102_meeting/1208.py
+++ 1102_meeting/1208.py
@@ -44,6 +44,21 @@ class Jamal(app_manager.RyuApp):
                 req = ofp_parser.OFPPortSftatsRequest(datapath, 0 , ofp.OFPP_ANY)
                 datapath.send_msg(req)
 
+        def add_flow(self,datapath,priority,match,actions,buffer_id=None):
+                ofproto = datapath.ofproto
+                parser = datapath.ofproto_parser
+                
+                inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                                     actions)]
+                if buffer_id:
+                        mod = parser.OFPFlowMod(datapath=datapath,buffer_id=buffer_id,
+                                                priority=priority,match=match,
+                                                instructions=inst)
+                else:
+                        mod = parser.OFPFlowMod(datapath=datapath,priority=priority,
+                                                match=match,instructions=inst)
+                datapath.send_msg(mod)
+        
         @set_ev_cls(ofp_event.EventOFPPortStatsReply,MAIN_DISPATCHER)
         def port_stats_reply_handler(self,ev):
                 msg = ev.msg
